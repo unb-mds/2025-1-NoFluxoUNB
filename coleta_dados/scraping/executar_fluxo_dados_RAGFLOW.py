9a76ab733dc763b384189278675c8332fb4b4e2e
staann <gustavoch2005@gmail.com>
2025-07-03 19:36:56 -0300

    alteração do caminho de pastas

--- coleta_dados/scraping/executar_fluxo_dados_RAGFLOW.py
+++ coleta_dados/scraping/executar_fluxo_dados_RAGFLOW.py
@@ -21,7 +21,7 @@ def gerar_nomes_pastas():
     semestre = 1 if hoje.month <= 6 else 2
     
     sufixo_semestre = f"{ano}_{semestre}"
-    diretorio_base = "dados"
+    diretorio_base = "coleta_dados/dados"
     
     nomes = {
         "dados_finais": os.path.join(diretorio_base, f"dados_finais_{sufixo_semestre}"),
